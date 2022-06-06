2c387c741778e82fab9221d5b35290b894b7631e
carmeloproetto <carmeloproetto7@gmail.com>
2022-06-06 10:05:49 +0200

    La camera segue il personaggio

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public float FollowSpeed = 2f;
-    public float yOffset =1f;
     public Transform target;
+    public Vector3 offset;
+    [Range(1,10)]
+    public float smoothFactor;
     
+    private void FixedUpdate(){
+        Follow();
+    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        Vector3 newPos = new Vector3(target.position.x, target.position.y + yOffset, -10f);
-        transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed*Time.deltaTime);
-    }  
+    void Follow(){
+        Vector3 targetPosition = target.position + offset;
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, targetPosition, smoothFactor*Time.fixedDeltaTime);
+        transform.position = targetPosition;
+    } 
 }
